9b98134c89b0b1976671a155eeb2069568bb18c6
wpc <alex.hal9000@gmail.com>
2012-06-13 22:29:29 -0700

    empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quantify::Application.config.secret_token = '809bd95bab06296df3243328203e3da2310ae1f9e772a22e8d623c43c509816c50072e83e2d6fa3518cdfa39a1172559c2430a7dc29bd501dd7ef63bce410d81'
