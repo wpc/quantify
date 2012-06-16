d2d96a1e6fbbf694fba23c2f89dd8298ad16dae7
wpc <alex.hal9000@gmail.com>
2012-06-16 00:23:44 -0700

    be able to list and add new measure

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,4 +2,17 @@ class FeaturesController < ApplicationController
   def index
     @features = @user.features
   end
+
+  def new
+    @feature = Feature.new
+  end
+
+  def create
+    @feature = @user.features.build(params[:feature])
+    if @feature.save
+      redirect_to features_url
+    else
+      render :new
+    end
+  end
 end
