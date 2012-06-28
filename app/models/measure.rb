7d403a5583c0a963a0b526566480c5f8846fceaf
wpc <alex.hal9000@gmail.com>
2012-06-28 00:32:20 -0700

    sort value of a measure by capture time

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,6 +1,6 @@
 class Measure < ActiveRecord::Base
   belongs_to :user
-  has_many :values
+  has_many :values, :order => "at DESC"
   
   attr_accessible :name
   validates_uniqueness_of :name, :scope => :user_id
